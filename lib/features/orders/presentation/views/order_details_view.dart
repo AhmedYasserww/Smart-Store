c0474259ad1bd815251175037b68237e689780d1
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2026-02-28 23:45:16 +0200

    create order details view

---
+++ lib/features/orders/presentation/views/order_details_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:smart_store/features/orders/presentation/views/widgets/order_details_view_body.dart';
+import 'package:smart_store/features/profile/presentation/views/widgets/custom_profile_app_bar.dart';
+class OrderDetailsView extends StatelessWidget {
+  const OrderDetailsView({super.key});
+  static const String routeName = 'order_details_view';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:CustomProfileAppBar(title: "Order Details"),
+      body: OrderDetailsViewBody(),
+    );
+  }
+}
