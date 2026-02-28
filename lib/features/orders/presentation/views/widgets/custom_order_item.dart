edcd7e303828180d68c976d29d908ec95e2723b5
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2026-02-28 23:42:04 +0200

    finish My orders view ui done

---
+++ lib/features/orders/presentation/views/widgets/custom_order_item.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:smart_store/core/widgets/custom_button.dart';
+import '../../../../../core/utils/app_color.dart';
+import '../../../../../core/utils/app_style.dart';
+
+class CustomOrderItem extends StatelessWidget {
+  const CustomOrderItem({
+    super.key,
+    required this.orderCode,
+    required this.status,
+  //  required this.onDetailsTap,
+  });
+
+  final String orderCode;
+  final String status;
+  //final VoidCallback onDetailsTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1,
+            color: AppColors.palletBorderColor,
+          ),
+          borderRadius:
+          BorderRadius.circular(16),
+        ),
+        shadows: const [
+          BoxShadow(
+            color: Color(0x1E000000),
+            blurRadius: 4,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment:
+        CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            'Order Code: $orderCode',
+            style: AppStyle.styleBold18,
+          ),
+
+          const SizedBox(height: 8),
+          Row(
+            children: [
+
+              Text(
+                'Status:',
+                style: AppStyle.styleGreyRegular14,
+              ),
+
+              const SizedBox(width: 4),
+
+              Text(
+                status,
+                style: AppStyle.styleGreyRegular14,
+              ),
+
+            ],
+          ),
+
+          const SizedBox(height: 8),
+
+          Align(
+            alignment: Alignment.centerRight,
+            child: SizedBox(
+              width: 90,
+              child: CustomButton(
+                text: "Details",
+                onTap: (){
+                  
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/orders/presentation/views/widgets/orders_view_body.dart
+++ lib/features/orders/presentation/views/widgets/orders_view_body.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_dimensions.dart';
+import 'custom_order_item.dart';
 class OrdersViewBody extends StatelessWidget {
   const OrdersViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal:
+        AppDimensions.homeScreenPadding,
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+
+          const SizedBox(height: 32),
+
+          CustomOrderItem(
+            orderCode: "123456",
+            status: "Pending",
+         //   onDetailsTap: () {},
+          ),
+
+          const SizedBox(height: 16),
+          CustomOrderItem(
+            orderCode: ":587496",
+            status: "Arrived",
+          //  onDetailsTap: () {},
+          ),
+
+        ],
+      ),
+    );
   }
 }
