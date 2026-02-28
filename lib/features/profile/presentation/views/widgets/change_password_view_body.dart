33acf8232df5508ee202439ee71116b3c85bf1c6
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2026-02-28 23:06:59 +0200

    finish change password view done

--- lib/features/profile/presentation/views/widgets/change_password_view_body.dart
+++ lib/features/profile/presentation/views/widgets/change_password_view_body.dart
@@ -1,9 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:smart_store/core/utils/app_dimensions.dart';
+import 'package:smart_store/core/widgets/custom_text_field.dart';
+import '../../../../../core/utils/app_style.dart';
+import 'edit_profile_view_widgets/custom_profile_action_button.dart';
+
 class ChangePasswordViewBody extends StatelessWidget {
   const ChangePasswordViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppDimensions.homeScreenPadding,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 32),
+          Text(
+            "Password",
+            style: AppStyle.styleRegular14,
+          ),
+
+          const SizedBox(height: 8),
+
+          CustomTextField(
+            hintText: "Enter your Password",
+            obscureText: true,
+          ),
+
+          const SizedBox(height: 16),
+
+          /// New Password
+          Text(
+            "New Password",
+            style: AppStyle.styleRegular14,
+          ),
+
+          const SizedBox(height: 8),
+
+          CustomTextField(
+            hintText: "Enter New Password",
+            obscureText: true,
+          ),
+
+          const SizedBox(height: 16),
+
+          /// Confirm Password
+          Text(
+            "Confirm Password",
+            style: AppStyle.styleRegular14,
+          ),
+
+          const SizedBox(height: 8),
+
+          CustomTextField(
+            hintText: "Confirm Password",
+            obscureText: true,
+          ),
+
+          const SizedBox(height: 16),
+
+          /// Buttons
+          CustomProfileActionButtons(
+            onDeleteTap: () {},
+            onSaveTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
