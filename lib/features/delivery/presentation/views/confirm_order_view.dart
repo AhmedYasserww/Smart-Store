25501079be0fb75bccb529f35b4718e9b305053f
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2026-05-12 03:28:03 +0300

    confirm order

--- lib/features/delivery/presentation/views/confirm_order_view.dart
+++ lib/features/delivery/presentation/views/confirm_order_view.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_store/features/delivery/presentation/views/widgets/confirm_order_view_body.dart';
 
-class ConfirmOrderView extends StatelessWidget {
+import '../../../bottom_nav_bar/presentation/views/bottom_nav_bar_view.dart';
+import '../../../cart/presentation/manager/get_cart_cubit/get_cart_cubit.dart';
+// في ConfirmOrderView — عمل clearCart هنا بعد ما الشاشة تفتح
+class ConfirmOrderView extends StatefulWidget {
   const ConfirmOrderView({super.key});
   static const String routeName = 'confirm_order_view';
 
   @override
+  State<ConfirmOrderView> createState() => _ConfirmOrderViewState();
+}
+
+class _ConfirmOrderViewState extends State<ConfirmOrderView> {
+  @override
+  void initState() {
+    super.initState();
+    // ✅ امسح الكارت بعد ما الشاشة تفتح
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<GetCartCubit>().clearCart();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ConfirmOrderViewBody(),
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (!didPop) {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            CustomNavigationBar.routeName,
+                (route) => false,
+          );
+        }
+      },
+      child: const Scaffold(
+        body: ConfirmOrderViewBody(),
+      ),
     );
   }
 }
-
-
